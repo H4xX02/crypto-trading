06e9c47e5d9fa85d0498030f0a91b7e32fd23445
H4xX02 <31244201+Loadboy@users.noreply.github.com>
2017-08-29 14:58:54 +0200

    upload

--- DailyMomentumStrat.py
+++ DailyMomentumStrat.py
@@ -1,3 +1,5 @@
+# following: https://www.youtube.com/watch?v=aaWLM60KxGg
+
 import tradingWithPython as twp
 import pandas as pd
 import numpy as np
@@ -17,13 +19,13 @@ print(df.tail())
 
 stratData = pd.DataFrame(index = df.index)
 stratData['daily_returns'] = df.Last.pct_change()*100
-stratData.plot()
+stratData.daily_returns.cumsum().plot()
 plt.show()
 print('Sharpe buy & hold', twp.sharpe(stratData.daily_returns))
 
 # initialize strategy idx
 
-idx = (stratData.daily_returns<-3).shift(1) &  (stratData.daily_returns <- 0) # find days that satisfy strategy
+idx = (stratData.daily_returns<-3).shift(1) #&  (stratData.daily_returns <- 1) # find days that satisfy strategy
 idx[0] = False # fill first entry with False due) NaN
 
 stratData['GoLong'] = idx
@@ -39,12 +41,12 @@ plt.show()
 
 # rewrite strategy to a single funtion
 
-def backtest(df, lag1 = -3, lag0 = 0):
+def backtest(df, lag1 = -3):							# , lag0 = -1
 	'''Function to backtest parameters'''
 	stratData = pd.DataFrame(index = df.index)
 	stratData['daily_returns'] = df.Last.pct_change()*100
 
-	idx = (stratData.daily_returns < lag1).shift(1) &  (stratData.daily_returns <- lag0) # find days that satisfy strategy
+	idx = (stratData.daily_returns < lag1).shift(1) #& (stratData.daily_returns < lag0) # find days that satisfy strategy
 	idx[0] = False # fill first entry with False due) NaN
 	stratData['GoLong'] = idx
 	stratData['pnl'] = 0 # innitialize pnl column with zeros
@@ -61,8 +63,33 @@ plt.show()
 
 # Scan all possible parameters
 
-lag1 = np.linspace(-10,10,30)
+par1 = np.linspace(-10,10,30)
 lag0 = np.linspace(-10,10,30)
+
+SH = np.zeros(len(par1))
+
+for i, cc in enumerate(par1):
+	pnl = backtest(df, lag1 = cc)
+	SH[i] = twp.sharpe(pnl)
+
+print(SH)
+
+i = np.unravel_index(SH.argmax(),SH.shape)
+SH[i]
+print('opt Threshold %.2f' % par1[i])
+
+print(par1[i])
+
+optimized_pnl = backtest(df, par1[i])
+print('Sharpe', twp.sharpe(optimized_pnl))
+optimized_pnl.cumsum().plot()
+plt.show()
+
+print(stratData.daily_returns.sum())
+print(optimized_pnl.sum())
+
+
+
 
 
 
