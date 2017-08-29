78172334cffddc386deba7d5970e576faa49b2f5
H4xX02 <31244201+Loadboy@users.noreply.github.com>
2017-08-29 14:11:39 +0200

    upload

--- DailyMomentumStrat.py
+++ DailyMomentumStrat.py
@@ -1,5 +1,6 @@
 import tradingWithPython as twp
 import pandas as pd
+import numpy as np
 import quandl
 import matplotlib.pyplot as plt
 
@@ -22,8 +23,9 @@ print('Sharpe buy & hold', twp.sharpe(stratData.daily_returns))
 
 # initialize strategy idx
 
-idx = (stratData.daily_returns<-3).shift(1) #&  (stratData.daily_returns <- 1) # find days that satisfy strategy
+idx = (stratData.daily_returns<-3).shift(1) &  (stratData.daily_returns <- 0) # find days that satisfy strategy
 idx[0] = False # fill first entry with False due) NaN
+
 stratData['GoLong'] = idx
 stratData['pnl'] = 0 # innitialize pnl column with zeros
 stratData['pnl'][idx] = stratData['daily_returns'][idx]
@@ -36,6 +38,31 @@ stratData.pnl.cumsum().plot()
 plt.show()
 
 # rewrite strategy to a single funtion
+
+def backtest(df, lag1 = -3, lag0 = 0):
+	'''Function to backtest parameters'''
+	stratData = pd.DataFrame(index = df.index)
+	stratData['daily_returns'] = df.Last.pct_change()*100
+
+	idx = (stratData.daily_returns < lag1).shift(1) &  (stratData.daily_returns <- lag0) # find days that satisfy strategy
+	idx[0] = False # fill first entry with False due) NaN
+	stratData['GoLong'] = idx
+	stratData['pnl'] = 0 # innitialize pnl column with zeros
+	stratData['pnl'][idx] = stratData['daily_returns'][idx]
+
+	return stratData['pnl']
+
+# Test of backtest function
+
+pnl = backtest(df)
+pnl.cumsum().plot()
+print('Sharpe', twp.sharpe(stratData.pnl))
+plt.show()
+
+# Scan all possible parameters
+
+lag1 = np.linspace(-10,10,30)
+lag0 = np.linspace(-10,10,30)
 
 
 
